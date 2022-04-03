72dfabeb20d47a9e1624ba650693dd9bc121dce4
Eka Awaludin <ekaawaludin@outlook.com>
2022-04-03 12:56:40 +0700

    Update imp-overide.js

--- js/imp-overide.js
+++ js/imp-overide.js
@@ -1,1 +1,1 @@
-$('html body div#frame-utama div#frame-konten div#frame-kxs div#konten div#Blog1.widget.Blog div.blog-posts.hfeed div.date-outer div.date-posts div.post-outer .post-body.entry-content amp-youtube.i-amphtml-layout-size-defined').attr('style', 'hidden !important');
+$('html body div#frame-utama div#frame-konten div#frame-kxs div#konten div#Blog1.widget.Blog div.blog-posts.hfeed div.date-outer div.date-posts div.post-outer .post-body.entry-content amp-youtube.i-amphtml-layout-size-defined').attr('style', 'overflow:inherit');
