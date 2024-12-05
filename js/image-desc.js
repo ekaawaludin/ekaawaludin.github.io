481dcceb01f929b37b5184089426e248aadcd72a
Eka Awaludin <ekaawaludin@live.com>
2024-12-05 07:41:37 +0700

    Delete js directory

--- js/image-desc.js
+++
@@ -1,5 +0,0 @@
-function copyDiv() {
-  var firstDivContent = document.getElementById('img-desc');
-  var secondDivContent = document.getElementById('img-desc-show');
-  secondDivContent.innerHTML = firstDivContent.innerHTML;
-}
--- js/imp-overide.js
+++
@@ -1,4 +0,0 @@
-let showElement = () => {
-	let ele = document.getElementByClassName('i-amphtml-layout-size-defined');
-    	ele.setAttribute ('style', 'overflow: inherit;');
-}
